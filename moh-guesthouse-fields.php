3d443f3ed16ee2254498950d89e36db7f796ee2b
Agfoghlaim <marieohara.gti@gmail.com>
2017-04-07 11:43:18 +0100

    meta updates wp_rooms custom table

--- moh-guesthouse-fields.php
+++ moh-guesthouse-fields.php
@@ -23,9 +23,11 @@ function moh_meta_callback($post){
 	//wp_nonce_field( basename( __FILE__ ), 'moh_rooms_nonce');
 	wp_nonce_field( 'moh_meta_save', 'moh_rooms_nonce');
 	//get_post_meta($id, $key, true or false for return a string or an array);
-	//global $post;
+
 	$moh_stored_meta = get_post_meta( $post->ID, '_room_id', true );
-	echo "<h1>" .  $post->ID . "</h1>";
+	$moh_stored_meta_room_type = get_post_meta( $post->ID, '_room_type', true );
+	$moh_stored_meta_room_rate = get_post_meta( $post->ID, '_room_rate', true );
+	echo "<h1> post id is: " .  $post->ID . "</h1>";
 	?>
 
 	<div id="moh-meta">
@@ -42,10 +44,19 @@ function moh_meta_callback($post){
 		</div>
 		<div class="meta-row">
 			<div class="meta-th">
-				<label for="room-rate" class="moh-row-title">Room Rate</label>	
+				<label for="room-type" class="moh-row-title">Room Type</label>	
 			</div>
 			<div class="meta-td">
-				<input type="text" name="room-rate" id="room-rate" value="">
+				<input type="text" name="room-type" id="room-type" value="<?php echo esc_attr( $moh_stored_meta_room_type ); ?>">
+			</div>
+		</div>
+		<div class="meta-row">
+			<div class="meta-th">
+				<label for="room-rate" class="moh-row-title">Room Rate</label>	
+				<small>enter room rate in cents €100 = 10000</small>
+			</div>
+			<div class="meta-td">
+				<input type="text" name="room-rate" id="room-rate" value="<?php echo esc_attr( $moh_stored_meta_room_rate ); ?>">
 			</div>
 		</div>
 	</div>
@@ -84,8 +95,60 @@ function moh_meta_save($post_id){
 		return;
 	}
 	$rm_id_data = sanitize_text_field($_POST['room-id'] );
+	$rm_type_data = sanitize_text_field($_POST['room-type']);
+	$rm_rate_data = sanitize_text_field($_POST['room-rate']);
+
 
 	update_post_meta($post_id, '_room_id', $rm_id_data);
+	update_post_meta($post_id, '_room_type', $rm_type_data);
+	update_post_meta($post_id, '_room_rate', $rm_rate_data);
+
+	//update wp_rooms table with same info
+	global $wpdb;
+	$tablename = $wpdb->prefix . 'rooms';
+	//chech wp_rooms
+	// $check_rooms="SELECT * FROM '$tablename'
+	// WHERE rm_id='$rm_id_data'";
+	// $num_rows = $wpdb->query($check_rooms); 
+	// if($num_rows >0){
+	// 	//update mysql
+	// 	$update_query = "UPDATE '$tablename'
+	// 	SET rm_type='$rm_type_data'
+	// 	WHERE rm_id='$rm_id_data";
+	// 	$wpdb->query($update_query); 
+	// }else{
+	 //  Insert a record works
+		// $newdata = array( 'rm_id' =>  $rm_id_data,
+  //                 		  'rm_type' => $rm_type_data); 
+		// $wpdb->insert($tablename , $newdata );
+
+//}
+	//update
+	$thedata = array(
+		'rm_type'=>$rm_type_data,
+	
+
+		);
+	$where = array(
+		'rm_id'=>$rm_id_data);
+	$wpdb->update( $tablename, $thedata, $where);       
+	// $check_rooms = $wpdb->get_results( 
+	// 	"SELECT rm_id, rm_type, amt_per_night 
+	// 	FROM 'wp_rooms'
+	// 	WHERE rm_id = '15'");
+
+	// foreach($check_rooms as $check_room){
+	// 	echo "<h1>" .  $check_room->rm_id . "</h1>";
+	// }
+	//$roomrow = $wpdb->get_row( "SELECT * FROM $wpdb->wp_rooms WHERE rm_id = $rm_id_data", ARRAY_A );
+	//var_dump($roomrow[1]);
+	  
+	//  //  Insert a record 
+	// $newdata = array( 'rm_id' =>  $rm_id_data,
+ //                  	'rm_type' => $rm_type_data); 
+	// $wpdb->insert($tablename , $newdata );  
+	
+	//$query = $wpdb->prepare("INSERT INTO ")
 	// $is_autosave = wp_is_post_autosave ($post_id);
 	// $is_revision = wp_is_post_revision($post_id);
 	// $is_valid_nouce = ( isset($_POST['moh_rooms_nonce']) && wp_verify_nonce($_POST['moh_rooms_nonce'], basename(__FILE__) )) ? 'true' : 'false';
